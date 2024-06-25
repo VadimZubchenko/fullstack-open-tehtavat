7e75d58ddbdd0793ca6c0cc488235d00e54a3b67
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-06-25 23:51:39 +0300

    tehtavat 1.3-1.5

---
+++ osa1/1.4/step1/src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
