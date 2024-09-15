0d52d712af651b0d5bf3743be6875ae2c82b26c3
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-09-15 21:46:20 +0300

    added proxy server

--- osa2/osa2-map-mine/src/service/notes.js
+++ osa2/osa2-map-mine/src/service/notes.js
@@ -3,16 +3,21 @@ const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  const nonExisting = {
-    id: 10000,
-    content: "This note is not saved to server",
-    important: true,
-  };
-  return request.then((response) => response.data.concat(nonExisting));
-  // return request.then((response) => {
-  //   return response.data;
-  // });
+  return request.then((response) => response.data);
 };
+
+// const getAll = () => {
+//   const request = axios.get(baseUrl);
+//   const nonExisting = {
+//     id: 10000,
+//     content: "This note is not saved to server",
+//     important: true,
+//   };
+//   return request.then((response) => response.data.concat(nonExisting));
+//   // return request.then((response) => {
+//   //   return response.data;
+//   // });
+// };
 
 const create = (newNote) => {
   const request = axios.post(baseUrl, newNote);
