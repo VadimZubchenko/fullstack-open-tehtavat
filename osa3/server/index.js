69355499f5f1a96d99809629f5febabb028d59eb
Artur Zubchenko <vadimzubchenko@hotmail.com>
2024-10-05 19:56:02 +0300

    Changes in all endpoints for Mongo Atlas

--- osa3/server/index.js
+++ osa3/server/index.js
@@ -1,12 +1,40 @@
-// Use .env with psw and port
-require("dotenv").config();
-
 // load server using Express.js
 const express = require("express");
 const app = express();
 
+// Use .env with psw and port
+require("dotenv").config();
+
 // Using a Mongoose setting as a module
 const Note = require("./models/note");
+
+// middleware for serve static files, where is index.html
+app.use(express.static("../dist"));
+
+// middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja.
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+// initiate middleware requestLogger
+app.use(requestLogger);
+
+// load json-parser using built-in middleware function in Express
+// se muuttaa JSON-muotoisen datan JavaScript-olioksi
+// ennen kuin routen käsittelijää kutsutaan.
+app.use(express.json());
+
+// Don't need 'cors ' in the version with static build in ./dist because front and back use the same origin
+const cors = require("cors");
+app.use(cors());
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 // API using MongoDB
 app.get("/", (req, resp) => resp.send("<h1>Hello World!</h1>"));
@@ -18,22 +46,19 @@ app.get("/api/notes", (request, response) => {
 });
 
 // GET requested note
-app.get("/api/notes/:id", (req, resp) => {
-  const id = req.params.id;
-  const note = Note.find();
-  if (note) {
-    resp.json(note);
-  } else {
-    resp.status(404).end();
-  }
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 // DELETE requested note
-app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  res.send(`Note with id: ${id} has been removed!`);
-  res.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateID = () => {
@@ -46,30 +71,28 @@ const generateID = () => {
 
 // POST new note
 app.post("/api/notes", (req, resp) => {
-  // !!! middleware 'app.use(json())' extracts body from request with
   const body = req.body;
 
   // check if body include content
-  if (!body.content) {
+  if (body.content === undefined) {
     return resp.status(400).json({ error: "content missing" });
   }
 
   // create note based on req body data
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateID(),
-  };
+  });
 
   // create notes including new note
-  notes = notes.concat(note);
-
-  resp.json(note);
+  note.save().then((savedNote) => {
+    resp.json(savedNote);
+  });
 });
 
 // middleware is exectuted after all end-ponts if no route handles the HTTP request.
-/* app.use(unknownEndpoint);
- */
+app.use(unknownEndpoint);
+
 const hostname = "0.0.0.0";
 const PORT = process.env.PORT;
 
@@ -125,41 +148,13 @@ const Note = mongoose.model("Note", noteSchema);
   },
 ]; */
 
-// middleware for serve static files, where is index.html
-/* app.use(express.static("../dist"));
-
-// middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja.
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-}; */
-
-// load json-parser using built-in middleware function in Express
-// se muuttaa JSON-muotoisen datan JavaScript-olioksi
-// ennen kuin routen käsittelijää kutsutaan.
-/* app.use(express.json()); */
-
-// initiate middleware requestLogger
-/* app.use(requestLogger); */
-
-// Don't need 'cors ' in the version with static build in ./dist because front and back use the same origin
-/* const cors = require("cors");
-app.use(cors()); */
-
-/* const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-}; */
-
 // there's a possible to use 'HTTP module' of Node.js
 /*const app = http.createServer((request, response) => {
   response.writeHead(200, { "content-type": "application/json" });
   response.end(JSON.stringify(notes));
 });*/
 
-//API using Epress.js
+//API using Express.js
 // GET all notes
 /* app.get("/", (req, resp) => resp.send("<h1>Hello World!</h1>"));
 
