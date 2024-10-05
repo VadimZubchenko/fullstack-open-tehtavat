8cb812505c7d53c1e19fbd913d85e7d173e4e14e
Artur Zubchenko <vadimzubchenko@hotmail.com>
2024-10-05 18:27:22 +0300

    Add connection to MongoDB Atlass

--- osa3/server/index.js
+++ osa3/server/index.js
@@ -1,77 +1,26 @@
-// load server using http module of Node.js
-/*const http = require("http");*/
+// Use .env with psw and port
+require("dotenv").config();
 
 // load server using Express.js
 const express = require("express");
 const app = express();
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-  {
-    id: "4",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+// Using a Mongoose setting as a module
+const Note = require("./models/note");
 
-// middleware for serve static files, where is index.html
-app.use(express.static("../dist"));
-
-// middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja.
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
-// load json-parser using built-in middleware function in Express
-// se muuttaa JSON-muotoisen datan JavaScript-olioksi
-// ennen kuin routen käsittelijää kutsutaan.
-app.use(express.json());
-
-// initiate middleware requestLogger
-app.use(requestLogger);
-
-// Don't need 'cors ' in the version with static build in ./dist because front and back use the same origin
-/* const cors = require("cors");
-app.use(cors()); */
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-// there's a possible to use 'HTTP module' of Node.js
-/*const app = http.createServer((request, response) => {
-  response.writeHead(200, { "content-type": "application/json" });
-  response.end(JSON.stringify(notes));
-});*/
-
-//API using Epress.js
-// GET all notes
+// API using MongoDB
 app.get("/", (req, resp) => resp.send("<h1>Hello World!</h1>"));
 
-app.get("/api/notes", (req, resp) => resp.json(notes));
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
 // GET requested note
 app.get("/api/notes/:id", (req, resp) => {
   const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
+  const note = Note.find();
   if (note) {
     resp.json(note);
   } else {
@@ -119,11 +68,108 @@ app.post("/api/notes", (req, resp) => {
 });
 
 // middleware is exectuted after all end-ponts if no route handles the HTTP request.
-app.use(unknownEndpoint);
-
+/* app.use(unknownEndpoint);
+ */
 const hostname = "0.0.0.0";
-const port = process.env.port || 3001;
+const PORT = process.env.port;
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
 });
+
+//////////////// --> Other realizations of server and database <-- ////////////////
+
+//load server using http module of Node.js
+/*const http = require("http");*/
+
+/* // ver. 3.3
+// load database using MongoDB Atlas
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+// ÄLÄ KOSKAAN TALLETA SALASANOJA GitHubiin!
+const url = `mongodb+srv://VadimZ:${password}@cluster0.g65io.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+ */
+// local database used in ver.2.0
+/* let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+  {
+    id: "4",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+]; */
+
+// middleware for serve static files, where is index.html
+/* app.use(express.static("../dist"));
+
+// middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja.
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+}; */
+
+// load json-parser using built-in middleware function in Express
+// se muuttaa JSON-muotoisen datan JavaScript-olioksi
+// ennen kuin routen käsittelijää kutsutaan.
+/* app.use(express.json()); */
+
+// initiate middleware requestLogger
+/* app.use(requestLogger); */
+
+// Don't need 'cors ' in the version with static build in ./dist because front and back use the same origin
+/* const cors = require("cors");
+app.use(cors()); */
+
+/* const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+}; */
+
+// there's a possible to use 'HTTP module' of Node.js
+/*const app = http.createServer((request, response) => {
+  response.writeHead(200, { "content-type": "application/json" });
+  response.end(JSON.stringify(notes));
+});*/
+
+//API using Epress.js
+// GET all notes
+/* app.get("/", (req, resp) => resp.send("<h1>Hello World!</h1>"));
+
+app.get("/api/notes", (req, resp) => resp.json(notes));  */
+
+// refactoring '_id' to 'id' and delete '__v' in returnedObject from Atlas
+/* noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(); // toString because_id is Object
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+}); */
--- osa3/server/mongo.js
+++ osa3/server/mongo.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://VadimZ:${password}@cluster0.g65io.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://VadimZ:${password}@cluster0.g65io.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
+// Define schema with parametries with type of data
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+// Create a model of schema
+const Note = mongoose.model("Note", noteSchema);
 
 // const note = new Note({
 //   content: 'Final note for Test',
@@ -30,9 +31,9 @@ const Note = mongoose.model('Note', noteSchema)
 //   mongoose.connection.close()
 // })
 
-Note.find({important: true}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
