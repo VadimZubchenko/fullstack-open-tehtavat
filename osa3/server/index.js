1932e2a3bacc1fac6ada710f77ddfc96d5861a50
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2024-11-13 13:19:32 +0200

    Fixed syntax erros by eslint

--- osa3/server/index.js
+++ osa3/server/index.js
@@ -1,106 +1,106 @@
 // load server using Express.js
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Use .env with psw and port
-require("dotenv").config();
+require('dotenv').config()
 
 // Using a Mongoose setting as a module
-const Note = require("./models/note");
+const Note = require('./models/note')
 
 // middleware for serve static files, where is index.html
-app.use(express.static("../dist"));
+app.use(express.static('../dist'))
 
 // middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja.
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.error("Error: ", error.message);
+  console.error('Error: ', error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // Don't need 'cors ' in the version with static build in ./dist because front and back use the same origin
-const cors = require("cors");
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
 // load json-parser using built-in middleware function in Express
 // se muuttaa JSON-muotoisen datan JavaScript-olioksi
 // ennen kuin routen käsittelijää kutsutaan.
-app.use(express.json());
+app.use(express.json())
 
 // initiate middleware requestLogger
-app.use(requestLogger);
+app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // API using MongoDB
-app.get("/", (req, resp) => resp.send("<h1>Hello World!</h1>"));
+app.get('/', (req, resp) => resp.send('<h1>Hello World!</h1>'))
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
 // GET requested note
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // POST new note
-app.post("/api/notes", (req, resp, next) => {
-  const body = req.body;
-  console.log("Content received:", body.content);
+app.post('/api/notes', (req, resp, next) => {
+  const body = req.body
+  console.log('Content received:', body.content)
 
   // create note based on req body data
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   // create notes including new note
   note
     .save()
     .then((savedNote) => {
-      resp.json(savedNote);
+      resp.json(savedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // DELETE requested note
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (request, response, next) => {
-  const { content, important } = request.body;
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
   /* const note = {
     content: body.content,
@@ -110,21 +110,21 @@ app.put("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedNote) => {
-      response.json(updatedNote);
+      response.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // middlewares are exectuted after all end-ponts if no route handles the HTTP request.
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const hostname = "0.0.0.0";
-const PORT = process.env.PORT;
+const hostname = '0.0.0.0'
+const PORT = process.env.PORT
 
 app.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}/`);
-});
+  console.log(`Server running at http://${hostname}:${PORT}/`)
+})
